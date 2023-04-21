ecaf07c4726a720686c6f9e2730e29d371697eb7
JD Taylor <jd.taylor@gmail.com>
2023-04-20 21:04:14 -0400

    start music sample read

--- scripts/potential_flow_viz_test.py
+++ scripts/potential_flow_viz_test.py
@@ -1,21 +1,74 @@
 from potentialflow import *
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import librosa
+# from scipy import signal
+# import soundfile 
 import time
+import subprocess, os
+
+################################################################################
+
+# samples_dir = '/home/jd/Documents/Miscellaneous/ames_music_visualizer/samples/'
+# sample_filename = samples_dir + 'Everbloom_2-4-23_Master_2.wav'
+
+sample_filename = librosa.ex('trumpet') # Short sample sound
+
+renders_dir = '/home/jd/Documents/Miscellaneous/ames_music_visualizer/renders/'
+render_filename = renders_dir + 'basic_animation.mp4'
+
+fps = 30 # Rendered viualization framerate
+dt = 1.0/fps # Delay between frames in seconds.
+dt_ms = dt*1000.0; # Delay in milliseconds
+
+cycle_duration_frames = 100
+
+#################################################################################
+
+y, sample_rate = librosa.load(sample_filename, mono=True, offset=0.0, duration=3.0)
+
+num_samples_raw = y.size
+
+sample_duration_sec = sample_duration_num_samples_raw/sample_rate
+
+print('sample_duration_num_samples_raw = ',sample_duration_num_samples_raw)
+print('sample_duration_sec = ',sample_duration_sed)
+
+fig, ax = plt.subplots()
+librosa.display.waveshow(y, sr=sample_rate, ax=ax)
+plt.show()
 
 
+hop_length = 1024
+D = librosa.amplitude_to_db(np.abs(librosa.stft(y, hop_length=hop_length)),ref=np.max)
+
+fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
+img = librosa.display.specshow(D, y_axis='log', sr=sample_rate, x_axis='time', ax=ax[1], hop_length=hop_length)
+
+ax[1].set(title='Log-frequency power spectrogram')
+ax[1].label_outer()
+fig.colorbar(img, ax=ax, format="%+2.f dB")
+
+plt.show()
+
+exit()
+
+#################################################################################
 
 x_points = np.linspace(-10, 10, 200)
-y_points = np.linspace(-10, 10, 200)
+y_points = np.linspace(-10, 10, 100)
 percentiles_to_include = 99.5
 
-
 field_0 = Flowfield([
-    Vortex(500,5.0,0.0),
-    Vortex(-500,-5.0,0.0),
-    Source(100,5.0,0.0),
-    Source(-100,-5.0,0.0),
-    Freestream(0.0,5.0) # Freestream Flow Up Slowly (lavalamp)
+    # Vortex(500,5.0,0.0),
+    # Vortex(-500,-5.0,0.0),
+    Vortex(500,5.0,5.0),
+    Vortex(500,-5.0,-5.0),
+    Vortex(-500,5.0,-5.0),
+    Vortex(-500,-5.0,5.0),
+    # Source(100,5.0,0.0),
+    # Source(-100,-5.0,0.0),
+    # Freestream(0.0,5.0) # Freestream Flow Up Slowly (lavalamp)
 ])
 
 X,Y,points = field_0.mesh_points(x_points,y_points)
@@ -24,38 +77,40 @@ streamfunction_0 = field_0.get_scalar("streamfunction", x_points, y_points)
 
 # field_0.draw("streamfunction", x_points, y_points)
 
-xp_0 = np.array([5.0, -5.0])
-yp_0 = np.array([-10.0, -10.0])
+# xp_0 = np.array([5.0, -5.0])
+# yp_0 = np.array([-10.0, -10.0])
+
+xp_0 = np.array([5.0, -5.0, 0.0, 0.0])
+yp_0 = np.array([0.0, 0.0, 5.0, -5.0])
 
 xp = xp_0
 yp = yp_0
 
 n_pts = xp.size
 
-fps = 100
-dt = 1/fps # Delay between frames in seconds.
-dt_ms = dt*1000.0; # Delay in milliseconds
 
-########################################################################
+# ################################################################################
 
 fig, ax = plt.subplots()
 
 ax.set_facecolor("black")
 ax.grid(False)
-# ax.set_axis_off()
-ax.margins(x=0)
+
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+
+fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
 
 scalar_min = np.nanpercentile(streamfunction_0, 50 - percentiles_to_include / 2)
 scalar_max = np.nanpercentile(streamfunction_0, 50 + percentiles_to_include / 2)
-counter_levels = np.linspace(scalar_min, scalar_max, 80)
+counter_levels = np.linspace(scalar_min, scalar_max, 40)
 
 c_data = ax.contour(X, Y, streamfunction_0.reshape(X.shape), 
                         levels=counter_levels,
-                        linelabels=False,
                         linewidths=0.5,
                         linestyles='solid',
                         alpha=1.0,
-                        cmap=plt.get_cmap("rainbow"),
+                        cmap=plt.get_cmap("jet"),
                     )
 
 
@@ -64,18 +119,25 @@ plot_data, = ax.plot(xp,yp,'ob')
 # def init():
 #     return plot_data
 
-def update(frame):
+def frame_update(frame):
 
-    global xp, yp, c_data, plot_data
+    global xp, yp, plot_data, c_data
+    global music_start, annotation1, annotation2
 
-    source_strength_i = 500*np.sin(2*np.pi*frame/100)+1
+    if frame == 0:
+        # music_thread.start()
+        music_start = time.perf_counter()
+
+    strength_i = 500*np.sin(2*np.pi*frame/cycle_duration_frames)+1
     
     field_i = Flowfield([
-        Vortex(source_strength_i,5.0,0.0),
-        Vortex(-source_strength_i,-5.0,0.0),
-        Source(source_strength_i,5.0,0.0),
-        Source(-source_strength_i,-5.0,0.0),
-        Freestream(0.0,1.0)
+        Vortex(strength_i,5.0,5.0),
+        Vortex(strength_i,-5.0,-5.0),
+        Vortex(-strength_i,5.0,-5.0),
+        Vortex(-strength_i,-5.0,5.0),
+        # Source(strength_i,5.0,0.0),
+        # Source(-strength_i,-5.0,0.0),
+        # Freestream(0.0,1.0)
     ])
 
     ###########################################################################
@@ -108,33 +170,21 @@ def update(frame):
     # replot frame contours
     c_data = ax.contour(X, Y, streamfunction_i.reshape(X.shape),
                             levels=counter_levels,
-                            linelabels=False,
                             linewidths=0.5,
                             linestyles='solid',
                             alpha=1.0,
                             cmap=plt.get_cmap("jet"),
                         )
 
-    return plot_data,c_data
+    return plot_data
 
-ani = animation.FuncAnimation(fig, update, frames=np.linspace(0, 100, 100), repeat=True)
+ani = animation.FuncAnimation(fig, frame_update, frames=cycle_duration_frames, repeat=False)
                     
-plt.show()
+ani.save(render_filename, fps=fps)
 
+plt.show(block=False)
 
+# Open rendered video in default system app (e.g. VLC)
+# subprocess.call(('xdg-open', render_filename))
 
-
-
-
-
-
-# To save the animation, use e.g.
-#
-# ani.save("movie.mp4")
-#
-# or
-#
-# writer = animation.FFMpegWriter(
-#     fps=15, metadata=dict(artist='Me'), bitrate=1800)
-# ani.save("movie.mp4", writer=writer)
 
